000f27990df9463015ecd2f21d08dfbf77d1a504
Claudiu Neamțu <xeosmarthome@gmail.com>
2021-08-16 15:30:37 +0300

    Folosim RNUI

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+import {ActionBar, AnimatedScanner, Colors, ChipsInput, Card} from "react-native-ui-lib";
+import icon from '../TitaniumPorcupine/assets/icon.png'
+import CardsScreen from "./src/Screens/TestScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hello, Timisoara!</Text>
+            <Text>OK</Text>
+
+            <CardsScreen/>
+
+            <ActionBar
+                centered
+                actions={[
+                    {label: 'Hide', onPress: () => Alert.alert('hide'), white: true},
+                    {label: 'Add Discount', onPress: () => Alert.alert('add discount'), white: true},
+                    {label: 'Duplicate', onPress: () => Alert.alert('duplicate'), white: true}
+                ]}
+                backgroundColor='orange'
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
