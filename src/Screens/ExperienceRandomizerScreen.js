a97a6485b14d90fb357bbb01ac61d17c17f4097c
Claudiu Neamțu <xeosmarthome@gmail.com>
2021-08-18 15:14:53 +0300

    Add flip-able card for experience randomizer

--- src/Screens/ExperienceRandomizerScreen.js
+++ src/Screens/ExperienceRandomizerScreen.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import {ActivityIndicator, Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {TimisoaraColors} from "../Style/colors";
 import {EXPERIENCES} from "../FakeBackend/Experiences";
+import CardFlip from "react-native-card-flip";
+import ExperienceCard from "../Components/ExperienceCard";
 
 
 const Title = () => {
@@ -13,44 +15,6 @@ const Title = () => {
 };
 
 
-const ExperiencePreview = ({experience}) => {
-    if (experience === null)
-        return (
-            <View style={styles.experiencePreview}>
-                <ActivityIndicator color={TimisoaraColors.MikadoYellow} size={100}/>
-                <Text style={styles.activityIndicatorText}>
-                    Cautand experiente noi
-                </Text>
-            </View>
-        )
-    return (
-        <TouchableOpacity
-            style={styles.experiencePreview}
-        >
-            <Image
-                source={{uri: experience.image}}
-                resizeMode={'cover'}
-                style={styles.experienceImage}
-            />
-            <View style={styles.experienceInfo}>
-                <View style={styles.experienceTitle}>
-                    <Text style={styles.experienceTitleText}>
-                        {
-                            experience.title
-                        }
-                    </Text>
-                </View>
-                <View style={styles.experiencePrice}>
-                    <Text style={styles.experiencePriceText}>
-                        {
-                            experience.price
-                        }
-                    </Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    )
-};
 
 
 const RandomExperienceButton = ({onPress}) => {
@@ -62,7 +26,7 @@ const RandomExperienceButton = ({onPress}) => {
             <Text
                 style={styles.buttonText}
             >
-                Recomanda experienta!
+                Cauta experienta!
             </Text>
         </TouchableOpacity>
     );
@@ -73,8 +37,6 @@ const ExperienceRandomizerScreen = () => {
     const [experience, setExperience] = useState(null);
 
     const onRandomExperienceButtonPress = () => {
-        if(experience === null)
-            return;
         setExperience(null);
         setTimeout(() => {
             const experienceIndex = Math.floor(Math.random() * EXPERIENCES.length);
@@ -90,8 +52,7 @@ const ExperienceRandomizerScreen = () => {
                 <Title/>
             </View>
             <View style={styles.experiencePreviewContainer}>
-                <ExperiencePreview experience={experience}/>
-
+                <ExperienceCard experience={experience}/>
             </View>
             <View style={styles.buttonContainer}>
                 <RandomExperienceButton onPress={onRandomExperienceButtonPress}/>
@@ -116,7 +77,6 @@ const styles = StyleSheet.create({
     experiencePreviewContainer: {
         flex: 3,
         width: '100%',
-        alignItems: 'center'
     },
     buttonContainer: {
         flex: 1,
@@ -136,61 +96,18 @@ const styles = StyleSheet.create({
         color: TimisoaraColors.RaisinBlack,
     },
 
-    activityIndicatorText: {
-        color: TimisoaraColors.MikadoYellow,
-        fontSize: 20,
-        alignSelf: 'center',
-        fontWeight: 'bold',
-        padding: 10
-    },
-    experiencePreview: {
-        width: '90%',
-        height: '100%',
-        borderColor: TimisoaraColors.RaisinBlack,
-        borderRadius: 20,
-        justifyContent: 'center'
-    },
-    experienceImage: {
-        flex: 3,
-        justifyContent: "center",
-        borderTopRightRadius: 30,
-        borderTopLeftRadius: 30
-    },
-    experienceInfo: {
-        flex: 1,
-        backgroundColor: TimisoaraColors.MikadoYellow,
-        borderBottomLeftRadius: 30,
-        borderBottomRightRadius: 30,
-    },
-    experienceTitle: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    experienceTitleText: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    experiencePrice: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    experiencePriceText: {
-        fontSize: 18
-    },
-
     button: {
         borderRadius: 50,
         backgroundColor: TimisoaraColors.MikadoYellow,
         paddingVertical: 10,
-        paddingHorizontal: 24
+        paddingHorizontal: 24,
+        elevation: 6
     },
     buttonText: {
         fontSize: 20,
         fontWeight: 'bold',
         color: TimisoaraColors.RaisinBlack
-    },
+    }
 });
 
 
